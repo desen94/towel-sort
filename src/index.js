d162120a8e1f049b86e0fa8a8436d6449f071ca1
desen <32864798+desen94@users.noreply.github.com>
2021-08-28 05:24:29 +0500

    refactor: fix error for auto-test into rsapp

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function towelSort(matrix) {
-    return matrix?.map((block, index) => index % 2 !== 0 ? block.reverse() : block).flat() || []
+    if (!matrix) return []
+    return matrix.map((block, index) => index % 2 !== 0 ? block.reverse() : block).flat()
 }
